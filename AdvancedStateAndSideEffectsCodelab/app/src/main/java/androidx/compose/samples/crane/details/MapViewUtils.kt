6a8228a5bcc50d7465a66a78225528e4184abbb3
“judrummer” <judrummercpe@gmail.com>
2022-12-10 21:37:35 +0700

    simplify AdvanceStateAndSideEffectCodeLab

--- AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/details/MapViewUtils.kt
+++ AdvancedStateAndSideEffectsCodelab/app/src/main/java/androidx/compose/samples/crane/details/MapViewUtils.kt
@@ -22,6 +22,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.samples.crane.R
 import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.google.android.libraries.maps.GoogleMap
 import com.google.android.libraries.maps.MapView
 
@@ -39,6 +41,20 @@ fun rememberMapViewWithLifecycle(): MapView {
         }
     }
 }
+
+// TODO: use this
+private fun getMapLifecycleObserver(mapView: MapView): LifecycleEventObserver =
+    LifecycleEventObserver { _, event ->
+        when (event) {
+            Lifecycle.Event.ON_CREATE -> mapView.onCreate(Bundle())
+            Lifecycle.Event.ON_START -> mapView.onStart()
+            Lifecycle.Event.ON_RESUME -> mapView.onResume()
+            Lifecycle.Event.ON_PAUSE -> mapView.onPause()
+            Lifecycle.Event.ON_STOP -> mapView.onStop()
+            Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
+            else -> throw IllegalStateException()
+        }
+    }
 
 fun GoogleMap.setZoom(
     @FloatRange(from = MinZoom.toDouble(), to = MaxZoom.toDouble()) zoom: Float
